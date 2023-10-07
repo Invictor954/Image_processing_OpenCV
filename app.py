edfc25cddb85c8788908ae98e164dd1252de80ed
Sebastian_Cespedes <sebastian.cespedes1@unmsm.edu.pe>
2023-10-07 00:47:35 -0500

    Definiendo la funcion ppmread para poder leer formato de imagenes ppm y agregando validaciones para asegurarnos que la imagen ppm es efectivamente una imagen ppm

---
+++ app.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import random
+import time
+
+
+cv2.imwrite('image.ppm', cv2.imread('image.png'))
+
+
+# Función para leer un archivo PPM en formato binario (P6)
+def ppmread(filename):
+    with open(filename, 'rb') as f:
+        # Leer el encabezado del archivo PPM
+        line = f.readline().decode('latin-1')
+        if not line.startswith('P6'):
+            print("ERROR: Expected PPM file to start with P6")
+            return None
+
+        line = f.readline().decode('latin-1')
+        dims = line.split()
+        width, height = int(dims[0]), int(dims[1])
+
+        line = f.readline().decode('latin-1')
+        if not line.startswith('255'):
+            print("ERROR: Expected 8-bit PPM with MAXVAL=255")
+            return None

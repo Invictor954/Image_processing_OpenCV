97409f7249cc14b20593be85a2269c097c8571f9
Diego de la Cruz <diego.delacruz@unmsm.edu.pe>
2023-10-07 01:22:45 -0500

    Agregando bucle en el array de del archivo ppm

--- draw_circle.py
+++ draw_circle.py
@@ -8,4 +8,14 @@ header = f'P6 {width} {height} {255}\n'
 img = array.array('B', [0, 0, 0] * width * height)
 opc=int(input())
 
+for n in range(0,opc):
+	ent = [int(x) for x in input().split()]
+	
+	X = ent[0]
+	Y = ent[1]
+	r = ent[2]
+	R = ent[3]
+	G = ent[4]
+	B = ent[5]
+
 
